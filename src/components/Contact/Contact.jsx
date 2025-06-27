b363a58237e7337be187f35d08278219212bce21
Jayindu <147903908+Jayindu@users.noreply.github.com>
2025-06-27 10:19:58 +0530

    Deploy-ready build with change error in github & linkdln

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -20,11 +20,11 @@ export const Contact = () => {
             src={getImageUrl("contact/linkedinIcon.png")}
             alt="LinkedIn icon"
           />
-          <a href="https://github.com/Jayindu">Jayindu Kandewaththa</a>
+          <a href="https://www.linkedin.com/in/jayindu-kandewaththa-253ba3266?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">Jayindu Kandewaththa</a>
         </li>
         <li className={styles.link}>
           <img src={getImageUrl("contact/githubIcon.png")} alt="Github icon" />
-          <a href="https://www.linkedin.com/in/jayindu-kandewaththa-253ba3266?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app">Jayindu Kandewaththa</a>
+          <a href="https://github.com/Jayindu">Jayindu Kandewaththa</a>
         </li>
       </ul>
     </footer>
